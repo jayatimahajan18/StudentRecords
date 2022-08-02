ee322f4cd74f0eed46ae5d6d3f169e4e7b5c5f2e
jayatmahajan <jayati.mahajan18@gmail.com>
2022-08-02 21:52:17 +0530

    First Draft with Basic Strucutre

---
+++ src/myPkg/Main.java
@@ -0,0 +1,40 @@
+package myPkg;
+
+/**
+ * 
+ * Goal: Create an application for student academic record management
+ * Features:
+ * 1. User is able to add student records
+ * 		Input: First Name, Last Name, Age, Gender, Marks for 5 Subjects, Roll Number
+ * 2.User is able to delete record of student by roll number
+ * 3. User is able to see list of all students with all details
+ * 		3.1 Some additional details should be displayed too
+ * 			a. Grade : If Perc > 90 : A
+ * 							>70-90: B
+ * 							>50-70 : C
+ * 							>33.3-50 :D 
+ * 							else: F
+ * 			b. Divison I : >60 II: 33.3-60 else NA
+ * 			c. Status: Pass/Fail
+ * 			d. Full Name
+ * 4. User is able to search student by:
+ * 		a. Roll Number
+ * 		b. Full Name
+ * 		c. Age Range[ Start and End ]
+ * 		d. Grade
+ * 		e. Status
+ * 5. Show Toppers [top 5 ]
+ * 6. Show Toppers for specfic Age [ex 15yrs]
+ * 		
+ * 
+ * @author ishug
+ *
+ */
+
+public class Main {
+	public static void main(String args[])
+	{
+		System.out.println("Hello World");
+		
+	}
+}
